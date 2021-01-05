428c0de8bffa5e6f29dd79f11f81a3ed8ac4d013
Lester Deeley <lester.deeley@yahoo.com>
2021-01-05 17:48:37 +0000

    Count duplicate characters in a string-functional style Stream and sort

--- src/CountDuplicateCharactersFunc.java
+++ src/CountDuplicateCharactersFunc.java
@@ -1,5 +1,7 @@
+import java.util.Comparator;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CountDuplicateCharactersFunc {
 
@@ -11,9 +13,13 @@ public class CountDuplicateCharactersFunc {
                         .mapToObj(c->(char) c)
                         .collect(Collectors.groupingBy(c->c, Collectors.counting()));
 
-        for (Character ch : characterIntegerMap.keySet()) {
-            System.out.println(ch + "=" + characterIntegerMap.get(ch));
-        }
+        characterIntegerMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))
+                .forEach(System.out::println);
+
+//        for (Character ch : characterIntegerMap.keySet()) {
+//            System.out.println(ch + "=" + characterIntegerMap.get(ch));
+//        }
 
     }
 
